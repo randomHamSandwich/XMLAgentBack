8c3a303204c1c785b8f2fc6cec6bc3234c89cf1e
RandomHamSandwich <basaric.aleksandar@gmail.com>
2020-05-10 17:26:05 +0200

    First commit

---
+++ agentBack20/src/main/java/com/xml/agentBack20/AgentBack20Application.java
@@ -0,0 +1,13 @@
+package com.xml.agentBack20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentBack20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentBack20Application.class, args);
+	}
+
+}
