e8aa6ba07572bec971e4f681f8782e64d9e388e1
Baske <basaric.aleksandar@gmail.com>
2020-05-24 12:40:18 +0200

    User can only access his info, postman with JWT cant access other users

--- agBa/src/main/java/com/xml/agBa/contriller/KorisnikController.java
+++ agBa/src/main/java/com/xml/agBa/contriller/KorisnikController.java
@@ -1,25 +1,27 @@
 package com.xml.agBa.contriller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.xml.agBa.service.KorisnikService;
 import com.xml.agBa.dto.KorisnikDTO;
-import com.xml.agBa.model.Korisnik;
+import com.xml.agBa.message.response.ResponseMessage;
 import com.xml.agBa.security.jwt.JwtAuthTokenFilter;
+import com.xml.agBa.security.service.UserDetailsImpl;
+import com.xml.agBa.service.KorisnikService;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -48,11 +50,13 @@ public class KorisnikController {
 //	
 //	
 	@GetMapping(value = "/kroisnik/{id}")
-	@PreAuthorize("hasAuthority('PACIJENT')")
-	public ResponseEntity<KorisnikDTO> getMyUserInfo(@PathVariable("email") Long id, @RequestHeader (name="Authorization") String token) {
-		JwtAuthTokenFilter f = new JwtAuthTokenFilter(); 
+	@PreAuthorize("hasAuthority('KORISNIK')")
+	public ResponseEntity<?> getMyUserInfo(@PathVariable("id") Long id, @AuthenticationPrincipal UserDetailsImpl  user) {
+		if(!user.getId().equals(id)) {
+			
+			return new ResponseEntity<>(new ResponseMessage("You can only access your information"), HttpStatus.FORBIDDEN);
+		}
 		KorisnikDTO korisnikListDTO = korisnikService.getUser(id);
-
 		return new ResponseEntity<>(korisnikListDTO, HttpStatus.OK);
 	}
 
@@ -65,4 +69,12 @@ public class KorisnikController {
 		return new ResponseEntity<>(noviKorisnik, HttpStatus.CREATED);
 	}
 	
+	@PutMapping
+	@PreAuthorize("hasAuthority('PACIJENT')")
+	public ResponseEntity<KorisnikDTO> changUserPremmsions(@PathVariable("id") Long id, @RequestHeader (name="Authorization") String token) {
+		JwtAuthTokenFilter f = new JwtAuthTokenFilter(); 
+		KorisnikDTO korisnikListDTO = korisnikService.getUser(id);
+
+		return new ResponseEntity<>(korisnikListDTO, HttpStatus.OK);
+	}	
 }
