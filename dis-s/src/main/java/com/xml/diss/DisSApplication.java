0a201f587c6c43b8f00e2d32376e8bbf4ca02d71
Baske <basaric.aleksandar@gmail.com>
2020-06-03 14:22:23 +0200

    Services are updated to match the model

---
+++ dis-s/src/main/java/com/xml/diss/DisSApplication.java
@@ -0,0 +1,13 @@
+package com.xml.diss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisSApplication.class, args);
+	}
+
+}
---
+++ dis-s/src/test/java/com/xml/diss/DisSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xml.diss;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisSApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
