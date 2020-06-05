3a6193f0bcef95540a9681504cb8565c4bbedb0c
Gamiraude <gamiraude@gmail.com>
2020-06-05 22:05:06 +0200

    CartList work in progress
    add list of reservations

--- agBa/src/main/java/com/xml/agBa/dto/CartDTO.java
+++ agBa/src/main/java/com/xml/agBa/dto/CartDTO.java
@@ -1,10 +1,81 @@
 package com.xml.agBa.dto;
 
-public class CartDTO {
-	
-	
-	public CartDTO() {
-		super();
-	}
+//import com.xml.agBa.model.Ad;
+//import com.xml.agBa.model.Cart;
 
+public class CartDTO {
+		
+//	private Long idAd;
+//	private Long idUser;
+//	private Long idCar;
+//	private String startDate;
+//	private String endDate;
+//	private Double priceForOneDay;
+//	private String discount;
+//	private Long car;
+	
+//	private Long endUser;
+	
+//	public CartDTO() {
+		
+//	}
+	
+//	public CartDTO(Long idAd, Long idUser, Long idCar , String startDate, String endDate, Double priceForOneDay, 
+//			String discount, Long car, Long endUser) {
+//		super();
+//		this.idAd = idAd;
+//		this.idUser = idUser;
+//		this.idCar = idCar;
+//		this.startDate = startDate;
+//		this.endDate = endDate;
+//		this.priceForOneDay = priceForOneDay;
+//		this.discount = discount;
+//		this.car = car;
+//		this.endUser = endUser;
+		
+//	}
+	
+//	public CartDTO(Cart newCart) {
+//		this.idAd= newCart.getIdAd();
+//		this.idUser = newCart.getIdUser();
+//		this.idCar = newCart.getIdCar();
+//		this.startDate = newCart.getStartDate().toString();
+//		this.endDate = newCart.getEndDate().toString();
+//		this.priceForOneDay = newCart.getPriceList().getPriceForOneDay();
+//		this.discount = newCart.getPriceList().getDiscounts().toString();
+//		this.car = newCart.getCar().getIdCar();
+//		this.endUser = newCart.getEndUser().getIdUser();
+//	}
+	
+//	public Long getIdAd() {
+//		return idAd;
+//	}
+	
+//	public Long getIdUser() {
+//		return idUser;
+//	}
+//	public Long getIdCar() {
+//		return idCar;
+//	}
+//	public String getStartDate() {
+//		return startDate;
+//	}
+//	public String getEndDate() {
+//		return endDate;
+//	}
+//	public Long getCar() {
+//		return car;
+//	}
+//	public Double getPriceForOneDay() {
+//		return priceForOneDay;
+//	}
+//	public String getDiscount() {
+//		return discount;
+//	}
+//	public Long getEndUser() {
+//		return endUser;
+//	}
+	
+	
+	
 }
--- agent-front/src/app/services/cart.service.ts
+++ agent-front/src/app/services/cart.service.ts
@@ -1,18 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
   })
   export class CartService {
-
-    private baseUrl = 'http://localhost:8080/api';
   
-    constructor(private http:HttpClient) {}
+    constructor() {}
   
    
-
-
-
   }
