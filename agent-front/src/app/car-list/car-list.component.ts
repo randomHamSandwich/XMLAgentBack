1e48997b616ef1159941fd2ba92f3b5ed1d4eaa1
Milan Krstin <mekserbia@gmail.com>
2020-06-15 15:58:26 +0200

    Napravljeno 90% funkcionalnosti za pregled slika i detalja vozila

--- agent-front/src/app/car-list/car-list.component.ts
+++ agent-front/src/app/car-list/car-list.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CarService } from '../services/car.service';
 import { Router } from '@angular/router';
+import { CarDTO } from '../car-create/CarDTO';
+import { CarDetailsComponent } from '../car-details/car-details.component';
 
 @Component({
   selector: 'app-car-list',
   templateUrl: './car-list.component.html',
-  styleUrls: ['./car-list.component.css']
+  styleUrls: ['./car-list.component.css'],
+  providers: [CarDetailsComponent]
 })
 export class CarListComponent implements OnInit {
   cars: any;
+  car: CarDTO;
   errorMessage: any;
 
   constructor(private carService: CarService,
-              private router: Router) { }
+              private router: Router,
+              private carDetailsComponent: CarDetailsComponent
+              ) 
+              { 
+                this.car = new CarDTO;
+                this.car.carBrand = "zastava";
+              }
 
   ngOnInit() {
     this.findAllCars();
@@ -36,5 +46,15 @@ export class CarListComponent implements OnInit {
     this.router.navigate(['/']);
   }
 
+  onDetails(c) {
+    // console.log("onDetail metoda")
+    this.car=c;
+    // console.log(this.car)
+    this.carDetailsComponent.fillACar(this.car);
+  }
+
+  returnCar() {
+    return this.car;
+  }
 
 }
