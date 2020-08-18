26bc5c3f10dfce475d872f3926fea5347c5bcfc9
Baske <basaric.aleksandar@gmail.com>
2020-08-18 23:09:10 +0200

    mail-s updated
    mail-s added to yml, eureka properies added, spring mail properties added,  registration mail uses String as parameter insted of UserDTO object, code for rabbitmq added but its not functional atm

--- mail-s/src/main/java/com/xml/mails/MailSApplication.java
+++ mail-s/src/main/java/com/xml/mails/MailSApplication.java
@@ -1,7 +1,12 @@
 package com.xml.mails;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MailSApplication {
@@ -9,5 +14,33 @@ public class MailSApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MailSApplication.class, args);
 	}
+	
+	
+	@Value("${myqueue}")
+	String queue;
+	
+	@Value("${myqueue2}")
+	String queue2;
+	
+	@Bean
+	Queue queue(){
+		return new Queue(queue,false);
+	}
+	
+	@Bean
+	Queue queue2(){
+		return new Queue(queue2,false);
+	}
+	
+	/*
+	 * Registrujemo bean koji ce sluziti za konekciju na RabbitMQ
+	 * gde se mi u primeru kacimo u lokalu.
+	 */
+	@Bean
+	public ConnectionFactory connectionFactory() {
+		CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+		return connectionFactory;
+	}
+	
 
 }
